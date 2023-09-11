b7b13321db122801fc065ea1e64e6b5230212937
Mitvi Shah <mitvi.webosmotic@gmail.com>
2023-09-11 15:24:29 +0530

    HTML CSS Responsive Website

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// function f1(){
+//     var a = document.getElementById("menu-btn");
+// var b=document.getElementsByTagName("li");
+// if(a.onclick){
+//     if(b.style.display == "none"){
+//         b.style.display ="block";    
+//     }
+//     else{
+//         b.style.display = "none";
+//     }
+// }
+// }
